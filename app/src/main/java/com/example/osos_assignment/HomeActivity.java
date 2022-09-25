563b12c4068ab8f261bd98e8de6f8617df538ceb
Mohammed Faiz <77430169+mdfaiz027@users.noreply.github.com>
2022-09-25 12:21:06 +0530

    First Commit

--- app/src/main/java/com/example/osos_assignment/HomeActivity.java
+++ app/src/main/java/com/example/osos_assignment/HomeActivity.java
@@ -82,6 +82,9 @@ public class HomeActivity extends AppCompatActivity /*implements OnMapReadyCallb
                         String lat = arrayMultipleObjectModel.getAddressObject().getGeoObject().getLat();
                         String lng = arrayMultipleObjectModel.getAddressObject().getGeoObject().getLng();
 
+                        latitude = Double.parseDouble(lat);
+                        longitude = Double.parseDouble(lng);
+
                         myModel = new MyModel(""+name, ""+username, ""+email, ""+street, ""+city, ""+zipcode);
                         arrayList.add(myModel);
                         recyclerAdapter.notifyDataSetChanged();
@@ -102,10 +105,8 @@ public class HomeActivity extends AppCompatActivity /*implements OnMapReadyCallb
         supportMapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(@NonNull GoogleMap googleMap) {
-                map = googleMap;
 
-                latitude = 17.387140;
-                longitude = 78.491684;
+                map = googleMap;
 
                 LatLng latlng = new LatLng(latitude, longitude);
                 MarkerOptions markerOptions = new MarkerOptions().position(latlng);
